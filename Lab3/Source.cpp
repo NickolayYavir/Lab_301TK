b3c6b0ae952faeca3a8d397c673e36cb2dbf325d
NickolayYavir <kolyayavir4@gmail.com>
2020-04-01 16:09:35 +0300

    redo

--- Lab3/Source.cpp
+++ Lab3/Source.cpp
@@ -3,23 +3,25 @@
 
 using namespace std;
 
-int main() 
-{
-	cout << "Input integer i = ";
-	int n;
-	cin >> n;
+int main() {
+	
 
+	int n;
 	double dblSum = 1;
 
-	cout << "( " << 1;
-	for (int i = 1, k = 2; i <= n; i++, k = k << 1) // k = 2^i
-	{
+	cout << "This program for solving expression like " << endl << "\t b = (1 + 1 / 2 + 1 / 2 ^ 2 + 1 / 2 ^ 3... + 1 / 2 ^ n) ^ n" << endl << endl;
+	cout << "Please input integer n = ";
+	cin >> n;
+	cout << endl << "Your expression " << endl << "( " << 1;
+
+	for (int i = 1, k = 2; i <= n; i++, k = k << 1) {	// k = 2^i
 		cout << " + (1 / 2)^" << i;
 		dblSum += (double) 1 / k;
 	}
+
 	dblSum = pow(dblSum, n);
 
-	cout << " )^" << n << endl << "Answer = " << dblSum << endl;
+	cout << " )^" << n << endl << endl << "Answer = " << dblSum << endl;
 	system("PAUSE");
 	return 0;
 }
--- Lab4/Source.cpp
+++ Lab4/Source.cpp
@@ -3,30 +3,31 @@
 
 using namespace std;
 
-int main() 
-{
-	const int nArray [SIZE][SIZE] = 
-	{
+int main() {
+
+	const int nArray[SIZE][SIZE] = {
 		{5,10,8},
 		{4,-1,9},
 		{-3,4,1}
 	};
-	
+
 	int min = nArray[0][0];
 
-	cout << "Array" << endl;
-	for (int i = 0; i < 3; i++) 
-	{
-		for (int j = 0; j < 3; j++) 
-		{
-			cout << nArray[i][j] << " ";
-			if (nArray[i][j] < min)
+	cout << "We have static array and we'll find smallest value." << endl << endl;
+	for (int i = 0; i < 3; i++) {
+
+		cout << "\t";
+		for (int j = 0; j < 3; j++) {
+
+			cout << nArray[i][j] << "\t";
+			if (nArray[i][j] < min) {
 				min = nArray[i][j];
+			}
 		}
-		cout << endl;
+		cout << endl << endl;
 	}
 
-	cout << endl << "min = " << min << endl;
+	cout << endl << "Smallest value = " << min << endl;
 	system("PAUSE");
 	return 0;
 }
